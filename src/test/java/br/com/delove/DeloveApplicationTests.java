007cbc70267c6139b2dba188443724806e0a05b5
Hugo Quadros <83436359+HugoQuadros@users.noreply.github.com>
2021-10-22 16:00:35 -0300

    commit inicial

---
+++ src/test/java/br/com/delove/DeloveApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.delove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeloveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
