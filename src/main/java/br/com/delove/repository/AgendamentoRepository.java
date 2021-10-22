4691d1e835002042c3c3354ec865ca2d6513387e
Patricia <pmiyazakii@gmail.com>
2021-10-22 17:37:09 -0300

    Criado repositórios

---
+++ src/main/java/br/com/delove/repository/AgendamentoRepository.java
@@ -0,0 +1,10 @@
+package br.com.delove.repository;
+
+import br.com.delove.model.Agendamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AgendamentoRepository extends JpaRepository<Agendamento,Long> {
+
+}
--- src/main/java/br/com/delove/repository/UsuarioRepository.java
+++ src/main/java/br/com/delove/repository/UsuarioRepository.java
@@ -1,4 +1,12 @@
 package br.com.delove.repository;
 
-public class UsuarioRepository {
+import br.com.delove.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    List<Usuario> findAllByCidadeContainingIgnoreCase (String cidade);
 }
