d203747a0b58bc0dd2f5caa677efa95abd27b6f9
LeandroMC <leandromc@pm.me>
2021-11-03 15:34:29 -0300

    remove spring security dependency

--- src/main/java/br/com/delove/DeloveApplication.java
+++ src/main/java/br/com/delove/DeloveApplication.java
@@ -2,9 +2,8 @@ package br.com.delove;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
 public class DeloveApplication {
 
 	public static void main(String[] args) {
