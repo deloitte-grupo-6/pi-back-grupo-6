9ece478e4be2c11cbdef7fc9320a5335d611deed
LeandroMC <leandromc@pm.me>
2021-11-05 11:51:09 -0300

    class import

--- src/main/java/br/com/delove/controller/UsuarioController.java
+++ src/main/java/br/com/delove/controller/UsuarioController.java
@@ -14,6 +14,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
