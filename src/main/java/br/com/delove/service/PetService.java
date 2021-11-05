a507856f3ecdc4ca4e25f98a97ad6cd017c15de7
LeandroMC <leandromc@pm.me>
2021-11-05 16:28:05 -0300

    revert last commit

--- src/main/java/br/com/delove/service/PetService.java
+++ src/main/java/br/com/delove/service/PetService.java
@@ -1,26 +1,22 @@
 package br.com.delove.service;
 
-import br.com.delove.model.Pet;
-import br.com.delove.model.Usuario;
-import br.com.delove.repository.PetRepository;
-import org.hibernate.event.internal.DefaultSaveOrUpdateEventListener;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import br.com.delove.model.Pet;
+import br.com.delove.repository.PetRepository;
 
 @Service
 public class PetService {
     @Autowired
     private PetRepository petRepository;
 
-    @Transactional
     public List<Pet> findPetByDisponibilidade(Boolean disponivel) {
         return petRepository.findAllByDisponivel(disponivel);
     }
 
-    @Transactional
     public Pet findPetById(Long id) {
         return petRepository.findById(id).orElseThrow(() -> new RuntimeException("Pet com o id " + id + " não existe"));
     }
@@ -29,12 +25,10 @@ public class PetService {
         return petRepository.findAllByEspecieAndDisponivel(especie, disponivel);
     }
 
-    @Transactional
     public List<Pet> listarPetsPorSexoEDisponibilidade(String sexo, boolean disponivel) {
         return petRepository.findAllBySexoAndDisponivel(sexo, disponivel);
     }
 
-    @Transactional
     public List<Pet> listarPetsPorRacaEDisponibilidade(String raca, boolean disponivel) {
         return petRepository.findAllByRacaAndDisponivel(raca, disponivel);
     }
