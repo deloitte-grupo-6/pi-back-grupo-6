5741a17ba2bc4c8cf80cb79ccf3669806b1ec052
LeandroMC <leandromc@pm.me>
2021-11-05 12:28:40 -0300

    add email property to usuarioSummaryModel

--- src/main/java/br/com/delove/summary/UsuarioSummaryModel.java
+++ src/main/java/br/com/delove/summary/UsuarioSummaryModel.java
@@ -5,6 +5,15 @@ public class UsuarioSummaryModel {
     private Long id;
     private String nome;
     private String contato;
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
     public Long getId() {
         return id;
