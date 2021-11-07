2d718e8b949e730893bcb8a5938134564e6b486a
LeandroMC <leandromc@pm.me>
2021-11-07 10:53:26 -0300

    fix lob stream exception

--- src/main/java/br/com/delove/service/UsuarioService.java
+++ src/main/java/br/com/delove/service/UsuarioService.java
@@ -5,11 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.delove.model.Usuario;
 import br.com.delove.repository.UsuarioRepository;
 
 @Service
+@Transactional
 public class UsuarioService {
     @Autowired
     private UsuarioRepository usuarioRepository;
